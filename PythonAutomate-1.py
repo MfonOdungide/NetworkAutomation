6fee25b9f6db6a5ae7120a7634140ac5f49290c4
Mfon Odungide <69343553+MfonOdungide@users.noreply.github.com>
2020-08-26 02:35:05 -0400

    Add files via upload

---
+++ PythonAutomate-1.py
@@ -0,0 +1,36 @@
+############################################################
+##############         ODUNGIDE, MFON         ################
+##############    Cisco Router Automation     ################
+##############           20 JUNE 2020         ################
+########################################################&#####
+## Program to configure a loopback address using telnet 
+#  
+# 
+#
+######################################################################
+
+######### global import statements  ###########
+
+import getpass
+import telnetlib
+
+HOST = "192.168.122.71"
+user = input("Enter your remote/telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+tn.write(b"int loop 0\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+print(tn.read_all().decode('ascii'))
